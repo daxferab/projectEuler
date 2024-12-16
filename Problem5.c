1c88a7974cacfdd66af0f895a8b2612811497d99
daxferab <daxferna@proton.me>
2024-12-16 20:09:26 +0100

    problem 5

---
+++ Problem5.c
@@ -0,0 +1,24 @@
+// 
+
+#include "MyLibrary.c"
+
+int divisible1to20(int num)
+{
+	for (int i = 1; i <= 20; i++)
+	{
+		if (!isMultiple(num, i))
+			return (0);
+	}
+	return (num);
+}
+
+int	main(void)
+{
+	int num = 1;
+	int smallestResult = 0;
+	while (smallestResult == 0)
+	{
+		smallestResult = divisible1to20(num++);
+	}
+	return (printf("%d\n", smallestResult));
+}
